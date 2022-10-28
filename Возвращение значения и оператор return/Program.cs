1729d4435d00996c4a1d752b7fdd0cc9677be718
Raziat <114012022+RaziatOmarova@users.noreply.github.com>
2022-10-28 03:49:37 +0300

    Добавьте файлы проекта.

---
+++ Возвращение значения и оператор return/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Возвращение_значения_и_оператор_return
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name()
+            {
+                return "Raziyat";
+            }
+            Console.WriteLine(name());
+
+            int Sum(int x, int y)
+            {
+                return x + y;
+            }
+            int result = Sum(478, 924);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
